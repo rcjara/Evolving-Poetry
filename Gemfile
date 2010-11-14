0a2f301d191081622136704c3feb9db03d1b3d94
Raul Jara <raul.c.jara@gmail.com>
2010-11-14 11:19:24 -0500

    Initial commit with an updated gemfile.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'nifty-generators', '>= 0.4.0'
+gem 'authlogic'
+
+
+group :development do
+  gem 'rspec-rails'
+  gem 'annotate-models'
+  gem 'faker'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'webrat'
+  gem 'factory_girl_rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EvolvingPoetry::Application.config.secret_token = 'ffc8d0fc01c33dd4dd6c2bdce85602c46805919a86f9bd2f53121902e4a4335c184265a946f068953be41d2cf83022094df795a4e480cde62c46fd33756bae25'
