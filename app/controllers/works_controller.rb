c5ded718b1df8f5385a3d10bd0db8e2378336ed7
Raul Jara <raul.c.jara@gmail.com>
2010-12-18 16:29:48 -0500

    some work on authors.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,9 +1,10 @@
 class WorksController < ApplicationController
   def show
     @work = Work.find(params[:id])
+    raise ActiveRecord::RecordNotFound unless @work.author.visible
 
     respond_to do |format|
-      format.html # show.html.erb
+        format.html # show.html.erb
     end
   end
 end
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -4,4 +4,16 @@ module AuthorsHelper
       "<li>" + link_to(work.title, work) + "</li>"
     end.join("\n").html_safe
   end
+
+  def authors_anchor_links(authors)
+    authors.collect do |a| 
+      "<li>" + author_anchor_link(a) + "</li>"
+    end.join("\n").html_safe
+  end
+
+  def author_anchor_link(author)
+    link_to author.full_name, :controller => 'authors',
+                              :action => 'index',
+                              :anchor => "author_#{author.id}"
+  end
 end
