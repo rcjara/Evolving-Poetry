edcc41f22d5b8223e2c7352efe0f42266ec46c7b
Raul Jara <raul.c.jara@gmail.com>
2012-12-30 13:56:40 -0500

    Remove unneccessary methods from MarkovHelper

--- spec/support/markov_helper.rb
+++ spec/support/markov_helper.rb
@@ -1,22 +1,6 @@
 module MarkovHelper
-  def array_of_tweets
-    File.open(File.expand_path(File.dirname(__FILE__) + '/config/Tweets')) {|f| f.map{|line| line.chomp} }
-  end
-
-  def array_of_cleaned_tweets
-    File.open(File.expand_path(File.dirname(__FILE__) + '/config/TweetsCleanedUp')) {|f| f.map{|line| line.chomp.toutf8} }
-  end
-
-  def array_of_processed_tweets
-    File.open(File.expand_path(File.dirname(__FILE__) + '/config/TestResults')) {|f| f.map{|line| line.chomp.toutf8} }
-  end
-
-  def nouns_to_markov_words(nouns)
-    nouns.collect { |noun| Markov::Word.new(noun, "the") }
-  end
-
   def poe_language
-    Markov::Language.new.tap do |lang|
+    @poe_language ||= Markov::Language.new.tap do |lang|
       text = File.read(File.expand_path(File.dirname(__FILE__) + '/../../lib/works/edgar_allan_poe.txt'))
       lang.add_snippet(text)
     end
